f9c518d9a822e940d9abd3a64c53ad976729c9d6
mvonderosten <markvonderosten@gmail.com>
2020-03-10 00:57:26 +0000

    03/09 second commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,8 +13,10 @@ const getState = ({ getStore, setStore }) => {
 					});
 			},
 
-			saveContact: contactToSave => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/markvon")
+			saveContact: contact => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/markvon", {
+					body: JSON.stringify(contact)
+				})
 					.then(resp => resp.json())
 					.then(data => {
 						console.log("saveContact", data);
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,24 +1,57 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-// import { Link, withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { AddContactCard } from "../component/AddContactCard.js";
+import { Context } from "../store/appContext";
 
 export const AddContact = () => {
 	const { store, actions } = useContext(Context);
+
+	// let contact = store.contacts[props.match.params.index];
+
 	const [contacts, setContacts] = useState();
+	const [fullName, setFullName] = useState();
+	const [phone, setPhone] = useState();
+	const [email, setEmail] = useState();
+	const [address, setAddress] = useState();
 
 	return (
-		<div>
-			<div
-				value={contacts}
-				onChange={e => setContacts(e.target.value)}
-				onKeyPress={e => {
-					if (e.key === "Enter") {
-						actions.saveContact();
-						setContacts("");
-					}
-				}}>
-				<AddContactCard change={item} />
+		<div className="container">
+			<div>
+				<h1 className="text-center mt-5">Add a new contact</h1>
+				<form>
+					<div className="form-group">
+						<label>Full Name</label>
+						<input type="text" className="form-control" placeholder="Full Name" />
+					</div>
+					<div className="form-group">
+						<label>Email</label>
+						<input type="email" className="form-control" placeholder="Enter email" />
+					</div>
+					<div className="form-group">
+						<label>Phone</label>
+						<input type="phone" className="form-control" placeholder="Enter phone" />
+					</div>
+					<div className="form-group">
+						<label>Address</label>
+						<input type="text" className="form-control" placeholder="Enter address" />
+					</div>
+					<button
+						type="button"
+						className="btn btn-primary form-control"
+						// onClick={() => {
+						//     actions.saveContact({
+						//         full_name: fullName,
+						//         email:
+						//     })
+						// }}
+					>
+						save
+					</button>
+					<Link className="mt-3 w-100 text-center" to="/">
+						or get back to contacts
+					</Link>
+				</form>
 			</div>
 		</div>
 	);
